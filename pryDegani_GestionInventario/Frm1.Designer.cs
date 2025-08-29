cb1ad0261996ff9b6104c0d524a2e4ca0567a0f9
facudegani-max <facudegani@gmail.com>
2025-08-29 13:25:45 -0300

    salir

--- pryDegani_GestionInventario/Frm1.Designer.cs
+++ pryDegani_GestionInventario/Frm1.Designer.cs
@@ -30,18 +30,19 @@
         {
             lbl1 = new Label();
             btnSalir = new Button();
+            btnSalir3 = new Button();
             SuspendLayout();
             // 
             // lbl1
             // 
             lbl1.AutoSize = true;
             lbl1.BorderStyle = BorderStyle.Fixed3D;
-            lbl1.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl1.Location = new Point(12, 25);
+            lbl1.Font = new Font("Segoe UI Black", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl1.Location = new Point(59, 57);
             lbl1.Name = "lbl1";
-            lbl1.Size = new Size(258, 19);
+            lbl1.Size = new Size(168, 39);
             lbl1.TabIndex = 0;
-            lbl1.Text = "Seleccione la opcion que quiere realizar";
+            lbl1.Text = "Bienvenido";
             lbl1.Click += label1_Click;
             // 
             // btnSalir
@@ -53,11 +54,21 @@
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
             // 
+            // btnSalir3
+            // 
+            btnSalir3.Location = new Point(101, 126);
+            btnSalir3.Name = "btnSalir3";
+            btnSalir3.Size = new Size(90, 29);
+            btnSalir3.TabIndex = 5;
+            btnSalir3.Text = "Salir";
+            btnSalir3.UseVisualStyleBackColor = true;
+            // 
             // Frm1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(292, 281);
+            Controls.Add(btnSalir3);
             Controls.Add(btnSalir);
             Controls.Add(lbl1);
             Name = "Frm1";
@@ -70,5 +81,6 @@
 
         private Label lbl1;
         private Button btnSalir;
+        private Button btnSalir3;
     }
 }
